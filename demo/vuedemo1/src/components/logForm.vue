f3948c628764cd694fd9129b99aa314c9eb514f2
Jaye118 <18330697972@163.com>
2017-12-20 16:32:47 +0800

    登录页动画

--- demo/vuedemo1/src/components/logForm.vue
+++ demo/vuedemo1/src/components/logForm.vue
@@ -13,14 +13,14 @@
         <div class="g-form-input">
           <input type="password" v-model="passwordModel" placeholder="请输入密码">
         </div>
-        <!--<span class="g-form-error">{{ passwordErrors.errorText }}</span>-->
+        <span class="g-form-error">{{ passwordErrors.errorText }}</span>
       </div>
       <div class="g-form-line">
         <div class="g-form-btn">
           <a class="button" @click="onLogin">登录</a>
         </div>
       </div>
-      <!--<p>{{ errorText }}</p>-->
+      <p>{{ errorText }}</p>
     </div>
   </div>
 </template>
@@ -30,7 +30,8 @@ export default {
   data () {
     return {
       usernameModel: '',
-      passwordModel: ''
+      passwordModel: '',
+      errorText:''
     }
   },
   computed: {
@@ -42,16 +43,60 @@ export default {
           }else{
               status = true;
               errorText = ''
-          }
+          };
+
+          //首次进入，userFlag是undefined，!undefined == true;手动输入时则是第二次进入，此时userFlag已赋值为true
+          if( !this.userFlag ){
+              errorText = '';
+              this.userFlag = true;
+          };
+
           return {
               status,
               errorText
-          }
+          };
+
+
+      },
+      passwordErrors(){
+          let status,errorText;
+              if(!/^\w{1,6}$/g.test(this.passwordModel)){
+                  status = false;
+                  errorText = '密码不是1-6位'
+              }else{
+                  status = true;
+                  errorText = ''
+              };
+              if( !this.passwordFlag ){
+                  errorText = '';
+                  this.passwordFlag = true;
+              };
+              return {
+                  status,
+                  errorText
+              }
       }
   },
   methods: {
       onLogin () {
-          console.log(this.usernameModel,this.passwordModel)
+          if( !this.userErrors.status || !this.passwordErrors.status ){
+              this.errorText = '部分选项未通过'
+          }else{
+              this.errorText = '';
+              console.log("logging");
+
+              //因未走后台请求，正确写法如下
+              /*this.$http.post('api/login')
+                  .then( (res) => {
+                      res.username = "yi";
+                      res.userId = "123456";
+                      this.$emit('has-log',res)
+                  }, (error) => {
+                      console.log(error)
+                  })
+              */
+              this.$emit('has-log');
+          }
       }
   }
 }
