adf025f2855f4580241562f2dab18558639352b4
Jaye118 <18330697972@163.com>
2018-03-24 14:39:55 +0800

    vue-倒计时

---
+++ demo/倒计时获取验证码/countDown.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <div id="example">
+    <button @click="send">
+      <span v-if="sendMsgDisabled">{{time+'秒后获取'}}</span>
+      <span v-if="!sendMsgDisabled">send</span>
+    </button>
+  </div>
+
+</template>
+
+<script>
+var vm = new Vue({
+    el: '#example',
+    data() {
+      return {
+        time: 60, // 发送验证码倒计时
+        sendMsgDisabled: false
+      }
+    },
+    methods: {
+      send() {
+        let me = this;
+        me.sendMsgDisabled = true;
+        let interval = window.setInterval(function() {
+          if ((me.time--) <= 0) {
+            me.time = 60;
+            me.sendMsgDisabled = false;
+            window.clearInterval(interval);
+          }
+        }, 1000);
+      }
+    }
+  })
+
+</script>
+
+<style>
+  button {
+    width: 100px;
+    height: 50px background: pink;
+  }
+</style>
+
+
+
+
+
